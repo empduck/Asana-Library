98e05ee3608ac26c8ffe54288f5f63ec28752344
empduck <josephwbell@gmail.com>
2016-03-18 19:47:36 -0400

    Initial Project

---
+++ Source/Asana Library/AsanaLibrary/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AsanaLibrary.Models
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty("workspaces")]
+        public Workspace[] Workspaces { get; set; }
+    }
+}
---
+++ Source/Asana Library/AsanaLibrary/Models/Workspace.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AsanaLibrary.Models
+{
+    public class Workspace
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("is_organization")]
+        public bool IsOrganization { get; set; }
+    }
+}
